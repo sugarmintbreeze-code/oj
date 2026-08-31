3af7ef6f225bf7f131e9b45b93c723841dc68496
Agri <sugar.mintbreeze@gmail.com>
2026-08-31 20:28:29 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'agrotour-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// ติดตั้ง Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// ดึงข้อมูลจาก Cache เมื่อไม่มีอินเทอร์เน็ต (Cache First Strategy)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
